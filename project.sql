2a5f1b7aff3e644c0b3150f94559df3a5e210dc9
Asingura Philip <asinguraphilip@gmail.com>
2025-04-04 22:24:55 +0300

    farmer,produce

--- project.sql
+++ project.sql
@@ -1,1 +1,23 @@
+create database granary_ug;
+
+use granary_ug;
+
+create table farmer(
+	farmer_id int primary key auto_increment,
+    name varchar(50),
+    contact varchar(40),
+    farm_location varchar(100),
+    registered_date date,
+    business_type varchar(50)
+);
+
+create table produce(
+	produce_id int primary key auto_increment,
+    name varchar(100),
+    type varchar(100),
+    storage_temperature_range varchar(100),
+    drying_requirements varchar(100),
+    farmer_id int,
+    foreign key(farmer_id) references farmer(farmer_id)
+);
 
